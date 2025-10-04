a7c4cc6755b597c1a93bbf5ec35549e57ace13d2
K413N <Kalen.Coolson@gmail.com>
2025-10-04 07:52:31 -0400

    metadata added and cached double blog loader

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
 import { MDXRemote } from 'next-mdx-remote/rsc';
-
 import { loadBlogPost } from '@/helpers/file-helpers';
-
 import BlogHero from '@/components/BlogHero';
-
 import styles from './postSlug.module.css';
+import { BLOG_TITLE } from '@/constants';
+
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(params.postSlug);
+  return {
+    title: `${frontmatter.title} - ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  }
+}
+
+
 
 
 async function BlogPost({ params }) {
